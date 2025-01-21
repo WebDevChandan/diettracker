8aa6e7e8f642f9d3cb23436584ddf144ab36bf06
WebDevChandan <hirechandan@gmail.com>
2025-01-22 02:09:15 +0530

    Add: Feat to add Breakfast in Diet Tracker

--- app/components/Breakfast.tsx
+++ app/components/Breakfast.tsx
@@ -1,12 +1,4 @@
 "use client"
-import { Button } from "@/components/ui/button";
-import { Dialog, DialogContent, DialogDescription, DialogFooter, DialogHeader, DialogTitle, DialogTrigger } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
-import { FaPlus } from "react-icons/fa";
-import { LuCirclePlus } from "react-icons/lu";
-// import { useDiet } from "../context/DietProvider";
 import { useDiet } from "../hook/useDiet";
 import DietTracker from "./DietTracker";
 import SubTitle from "./SubTitle";
@@ -14,13 +6,10 @@ import SubTitle from "./SubTitle";
 export default function Breakfast() {
   const { breakfast } = useDiet();
 
-  console.log("Got BreakFAst?");
-  console.log(breakfast);
   return (
     <>
       <SubTitle label="Breakfast" />
-      {/* <DietTracker diet={breakfast} /> */}
+      <DietTracker diet={breakfast} />
     </>
-
   )
 }
