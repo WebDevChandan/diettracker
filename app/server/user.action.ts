c8c9dea79b60d347fc242e0b15808d3e4fed1b2e
WebDevChandan <hirechandan@gmail.com>
2025-02-11 11:45:35 +0530

    Add debugging logs to user creation process; enhance visibility during user creation

--- app/server/user.action.ts
+++ app/server/user.action.ts
@@ -2,6 +2,9 @@ import { UserType } from "@/types/User";
 import prisma from "@/utils/prisma";
 
 export const createUser = async (user: UserType) => {
+    debugger;
+    console.log("createUser");
+    console.log(user);
     try {
         await prisma.user.create({
             data: {
